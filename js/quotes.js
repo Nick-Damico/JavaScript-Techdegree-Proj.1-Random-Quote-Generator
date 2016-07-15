f8c407c3499a991fc554f583dd79d13cdc3e1cea
Nicholas D'Amico <nickalan82@icloud.com>
2016-07-14 21:39:13 -0400

    Quotes..js, clean up comments
    -Array of Quotes moved to separate js file.
    - <script> tag to quotes.js added
    - <removed quotes array from script.js
    - Changes to css/styles.css

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+
+var quotes = [
+  {
+    quote : "Wilderness is not a luxury but necessity of the human spirit.",
+    source : "Edward Abbey",
+    citation : "Desert Solitaire",
+    year : 1968
+  },
+  {
+    quote : "We do not see nature with our eyes, but with our understandings and our hearts.",
+    source : "William Hazlett",
+    citation : "Thoughts on Taste, Edinburgh Magazine",
+    year : 1818
+  },
+  {
+    quote : "Afoot and lighthearted I take to the open road, healthy, free, the world before me. Henceforth, I ask not good fortune, I myself am good fortune. Henceforth, I whimper no more, postpone no more, need nothing.",
+    source : "Walt Whitman",
+    citation : "Songs for the Open Road",
+    year : 1998
+  },
+  {
+    quote : 'I felt my lungs inflate with the onrush of scenery - air, mountains, trees, people. I thought, "this is what it is to be happy."',
+    source : "Sylvia Plath",
+    citation : "The Bell Jar",
+    year : 1963
+  },
+  {
+    quote : "Not just beautiful, though - the stars are like the trees in the forest, alive and breathing. And they're watching me.",
+    source : "Haruki Murakami",
+    citation : "Kafka on the Shore",
+    year : 2002
+  },
+  {
+    quote : "if we Surrendered to earth's intelligence we could rise up rooted, like trees.",
+    source : "Rainer Maria Rilke",
+    citation : "Rilke's Book of Hours",
+    year : 1905
+  },
+  {
+    quote : "Heaven is under our feet as well as over our heads.",
+    source : "Henery David Thoreau",
+    citation : "Walden",
+    year : 1854
+  },
+  {
+    quote : "I wonder if the snow loves the trees and fields, that it kisses them so gently? And then it covers them up them up snug, you know, with a white quilt; and perphaps it says 'Go to sleep, darlings, till the summer comes again.'",
+    source : "Lewis Carroll",
+    citation : "Alice's Adventures in Wonderland & Through the Looking Glass",
+    year : 1865
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* Random Quote Machine. by Nicholas D'Amico @ nicholasdamico.net */
+
 /* This program generates one of five random quotes when a user clicks the "Show another quote" button on the web page. The Quote is then printed to the web page. If no user input from clicking the button occurs then every 12 secs a new quote will be randomly selected and printed to the DOM.
  */
 
@@ -8,26 +9,6 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 var viewedQuotes = [];  // Array to store randomly selected Quotes.
 var intervalID;
-
-// Array of Objects for Quote Machine
-var quotes = [{
-    quote: "Wilderness is not a luxury but necessity of the human spirit.",
-    source: "Edward Abbey",
-    citation: "Desert Solitaire",
-    year: 1968
-}, {
-    quote: "We do not see nature with our eyes, but with our understandings and our hearts.",
-    source: "William Hazlett",
-    citation: "Thoughts on Taste, Edinburgh Magazine",
-    year: 1818
-}, {
-    quote: "Afoot and lighthearted I take to the open road, healthy, free, the world before me. Henceforth, I ask not good fortune, I myself am good fortune. Henceforth, I whimper no more, postpone no more, need nothing.",
-    source: "Walt Whitman",
-    citation: "Songs for the Open Road",
-    year: 1998
-}];
-
-
 
 // randomly generates and returns a number.
 function randomNum(upper) {
@@ -44,34 +25,48 @@ function printQuote() {
     var message = '<p class="quote">' + selectedQuote.quote + '</p>';
     message += '<p class="source">' + selectedQuote.source;
 
-    if (selectedQuote.hasOwnProperty('citation')) {                             // Checks for 'citation' property in Object.
-        message += '<span class="citation">' + selectedQuote.citation +          // Only adds HTML <span> if Obj. has 'citation' property.
+// Checks for 'citation' property in Object.
+// Only adds HTML <span> if Obj. has 'citation' property.
+    if (selectedQuote.hasOwnProperty('citation')) {
+        message += '<span class="citation">' + selectedQuote.citation +
         '</span>';
     }
 
-    if (selectedQuote.hasOwnProperty('year')) {                                 // Checks for 'year' property in Object.
-        message += ' <span class="year">' + selectedQuote.year + ' </span>';    // Only adds HTML <span> if Obj. has 'year' property.
+// Checks for 'year' property in Object.
+// Only adds HTML <span> if Obj. has 'year' property.
+    if (selectedQuote.hasOwnProperty('year')) {
+        message += ' <span class="year">' + selectedQuote.year + ' </span>';
     }
     message += ' </p>';
 
+ // Compiled HTML 'message' is sent to DOM replaces HTML on element ID 'quote-box'.
     document.getElementById("main-body").style.background = backgrdColor;
-    document.getElementById("quote-box").innerHTML = message;                   // Compiled HTML 'message' is sent to DOM replaces HTML on element ID 'quote-box'.
+    document.getElementById("quote-box").innerHTML = message;
 }
 
-function getRandomQuote() {                                                     // Function generates randomly selected Quote.
-    var ranNumber = Math.floor(Math.random() * quotes.length);                  // Random number generated based on quote arrays length.
-    var spliceQuote = quotes.splice(ranNumber, 1)[0];                           // Removes (1) object from quotes array stores in spliceQuote.
-                                                                                // Makes sure no one quote
+// Function generates randomly selected Quote.
+function getRandomQuote() {
+    // Random number generated based on quote arrays length.
+    var ranNumber = Math.floor(Math.random() * quotes.length);
+
+    // Removes (1) object from quotes array stores in spliceQuote.
+    var spliceQuote = quotes.splice(ranNumber, 1)[0];
+
+    // Checks to see if quotes array is empty.
+    // If empty, moves all removed quote objects back to quotes array.
     if (quotes.length === 0) {
-        quotes = viewedQuotes;                                                  // Checks to see if quotes array is empty.
-        viewedQuotes = [];                                                      // If empty, moves all removed quote objects back to quotes array.
+        quotes = viewedQuotes;
+        viewedQuotes = [];
     }
 
-    viewedQuotes.push(spliceQuote);                                             // Adds spliced quote to back of viewedQuotes array.
-    return spliceQuote;                                                         // returns random quote to printQuote();
+    // Adds spliced quote to back of viewedQuotes array.
+    // returns random quote to printQuote();
+    viewedQuotes.push(spliceQuote);
+    return spliceQuote;
 }
 
-function randomColor() {                                                        // Generates random rgb color and formats color for css value.
+// Generates random rgb color and formats color for css value.
+function randomColor() {
   var rColor = 'rgb(' + randomNum(256) + ', ';
   rColor += randomNum(256) + ', ';
   rColor += randomNum(256) + ')';
