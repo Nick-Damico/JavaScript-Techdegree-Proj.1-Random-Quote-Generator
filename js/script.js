34a7b51d60280578028928426cce6b89862b75a9
Nicholas D'Amico <nickalan82@icloud.com>
2016-07-15 22:32:00 -0400

    Console.log and Formatting
    - Added console.log(‘Start’) and (‘End’) to project.
    -cleaned up format of code.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 /* Random Quote Machine. by Nicholas D'Amico @ nicholasdamico.net */
 
-/* This program generates one of five random quotes when a user clicks the "Show another quote" button on the web page. The Quote is then printed to the web page. If no user input from clicking the button occurs then every 12 secs a new quote will be randomly selected and printed to the DOM.
+/* Program Cross Browser Compatiability Checked in:
+  - Chrome Version 51.0.2704.103 - .106 (64-bit)
+  - Firefox 39.0 - FireFox 47.0.1
+  - Safari 9.1.1
+
+
+/* This program generates random quotes when a user clicks the "Show another quote" button on the web page. The Quote is then printed to the web page with a new random background-color. If no user input from clicking the button occurs then every 15 secs a new quote will be randomly selected and printed to the DOM.
  */
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+console.log('Start Program');
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-var viewedQuotes = [];  // Array to store randomly selected Quotes.
+var viewedQuotes = []; // Array to store randomly selected Quotes.
 var intervalID;
 
 // randomly generates and returns a number.
@@ -25,21 +32,21 @@ function printQuote() {
     var message = '<p class="quote">' + selectedQuote.quote + '</p>';
     message += '<p class="source">' + selectedQuote.source;
 
-// Checks for 'citation' property in Object.
-// Only adds HTML <span> if Obj. has 'citation' property.
+    // Checks for 'citation' property in Object.
+    // Only adds HTML <span> if Obj. has 'citation' property.
     if (selectedQuote.hasOwnProperty('citation')) {
         message += '<span class="citation">' + selectedQuote.citation +
-        '</span>';
+            '</span>';
     }
 
-// Checks for 'year' property in Object.
-// Only adds HTML <span> if Obj. has 'year' property.
+    // Checks for 'year' property in Object.
+    // Only adds HTML <span> if Obj. has 'year' property.
     if (selectedQuote.hasOwnProperty('year')) {
         message += ' <span class="year">' + selectedQuote.year + ' </span>';
     }
     message += ' </p>';
 
- // Compiled HTML 'message' is sent to DOM replaces HTML on element ID 'quote-box'.
+    // Compiled HTML 'message' is sent to DOM replaces HTML on element ID 'quote-box'.
     document.getElementById("main-body").style.background = backgrdColor;
     document.getElementById("quote-box").innerHTML = message;
 }
@@ -65,17 +72,18 @@ function getRandomQuote() {
     return spliceQuote;
 }
 
-// Generates random rgb color and formats color for css value.
+// Generates random rgb color and formats color for css rgb value.
 function randomColor() {
-  var rColor = 'rgb(' + randomNum(256) + ', ';
-  rColor += randomNum(256) + ', ';
-  rColor += randomNum(256) + ')';
-  return rColor;
+    var rColor = 'rgb(' + randomNum(256) + ', ';
+    rColor += randomNum(256) + ', ';
+    rColor += randomNum(256) + ')';
+    return rColor;
 }
 
 // Used on <body onload="changeQuote();".
 // Sets a 15second interval call to printQuote().
 // every 15 secs a new quote and background color will occur.
 function changeQuote() {
-  intervalID = setInterval(printQuote, 15000);
+    intervalID = setInterval(printQuote, 15000);
 }
+console.log('End Program');
