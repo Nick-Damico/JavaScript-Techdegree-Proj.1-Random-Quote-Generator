4dc135a664ea18dd0421c2cdf3b2e3868bea61bb
Nicholas D'Amico <nickalan82@icloud.com>
2016-07-15 22:07:03 -0400

    Tags Array
    descriptive tags property/ values added to quotes.js.

--- js/quotes.js
+++ js/quotes.js
@@ -4,48 +4,56 @@ var quotes = [
     quote : "Wilderness is not a luxury but necessity of the human spirit.",
     source : "Edward Abbey",
     citation : "Desert Solitaire",
-    year : 1968
+    year : 1968,
+    tags : ["inspirational", "nature", "wilderness"]
   },
   {
     quote : "We do not see nature with our eyes, but with our understandings and our hearts.",
     source : "William Hazlett",
     citation : "Thoughts on Taste, Edinburgh Magazine",
-    year : 1818
+    year : 1818,
+    tags : ["inspirational", "nature"]
   },
   {
     quote : "Afoot and lighthearted I take to the open road, healthy, free, the world before me. Henceforth, I ask not good fortune, I myself am good fortune. Henceforth, I whimper no more, postpone no more, need nothing.",
     source : "Walt Whitman",
     citation : "Songs for the Open Road",
-    year : 1998
+    year : 1998,
+    tags : ["inspirational", "nature", "adventure", "wise"]
   },
   {
     quote : 'I felt my lungs inflate with the onrush of scenery - air, mountains, trees, people. I thought, "this is what it is to be happy."',
     source : "Sylvia Plath",
     citation : "The Bell Jar",
-    year : 1963
+    year : 1963,
+    tags : ["nature", "outside", "inspirational"]
   },
   {
     quote : "Not just beautiful, though - the stars are like the trees in the forest, alive and breathing. And they're watching me.",
     source : "Haruki Murakami",
     citation : "Kafka on the Shore",
-    year : 2002
+    year : 2002,
+    tags : ["beautiful", "forest", "thoughful", "stars", "nature"]
   },
   {
     quote : "if we Surrendered to earth's intelligence we could rise up rooted, like trees.",
     source : "Rainer Maria Rilke",
     citation : "Rilke's Book of Hours",
-    year : 1905
+    year : 1905,
+    tags : ["thoughful", "stars", "nature"]
   },
   {
     quote : "Heaven is under our feet as well as over our heads.",
     source : "Henery David Thoreau",
     citation : "Walden",
-    year : 1854
+    year : 1854,
+    tags : ["wise", "nature", "thoughful"]
   },
   {
     quote : "I wonder if the snow loves the trees and fields, that it kisses them so gently? And then it covers them up them up snug, you know, with a white quilt; and perphaps it says 'Go to sleep, darlings, till the summer comes again.'",
     source : "Lewis Carroll",
     citation : "Alice's Adventures in Wonderland & Through the Looking Glass",
-    year : 1865
+    year : 1865,
+    tags : ["beautiful", "forest", "thoughful", "nature", "loving"]
   }
 ];
